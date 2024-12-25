d2f78ab4f81191456517779a582e5c9684a6ca05
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2024-12-25 04:00:16 -0600

    YIPPE ACTUAL CODE IS DONE

--- Crush.py
+++ Crush.py
@@ -30,20 +30,21 @@ def ask_question(prompt, options=None):
             return response
         print(f"\nPlease choose from the following: {', '.join(options)}\n")
 
-def no():
-    print('\nDon\'t tell them. \n')
+def end():
     time.sleep(3)
     exit()
+
+def no():
+    print('\nDon\'t tell them. \n')
+    end()
 
 def no2():
     print('\nDon\'t tell them because it\'s going to run the purpose they serve in your life rn. \n')
-    time.sleep(3)
-    exit()
+    end()
 
 def yes():
     print('\nTell them. \n')
-    time.sleep(3)
-    exit()
+    end()
 
 def idfk():
     question = ask_question('\nDoes the potential of getting together with your crush and/or potentially breaking up with them outweight the risk of potentially changing the dynamic of the friend group forever? \n\n', ['yes', 'no'])
@@ -54,13 +55,11 @@ def idfk():
 
 def idk():
     print('\nYeaaah do it, as long as your ok with the fact that getting together with your crush and/or potentially breaking up with them may/will change the dynamic of the friend group forever/breaking the friend group entirely. \n')
-    time.sleep(3)
-    exit()
+    end()
 
 def idek():
     print('\nI meaannnn I wouldn\'t tbh... \n')
-    time.sleep(3)
-    exit()
+    end()
 
 def calculate_cady_law():
     print('\nIs it fucked up for you to ask out your friend\'s ex? \n')
@@ -88,12 +87,10 @@ def calculate_cady_law():
 
     if g < 2:
         print(f"\nBro really, only {g} months since they broke up? Don't even think about it. \n")
-        time.sleep(3)
-        exit()
+        end()
     elif t > g:
         print(f"\n{g} months you say? Well, you're cutting it close. Wait it out. \n")
-        time.sleep(3)
-        exit()
+        end()
     elif t <= g:
         while True:
             response = ask_question('\nDo either of them still have feelings for each other? \n\n', ['yes', 'no', 'idk'])
@@ -111,21 +108,27 @@ def idrk():
     if in_group == 'yes':
         dated_in_group = ask_question('\nWas your crush ever seeing someone else in your friend group? \n\n', ['yes', 'no'])
         if dated_in_group == 'yes':
-            question = ask_question('\nWas your crush casually hooking up with someone in your friend group (1) or was your crush dating someone else in the friend group (2)? \n\n',['1','2'])
+            question = ask_question('\nWas your crush casually hooking up with someone in your friend group (1) or was your crush dating someone else in the friend group? (2) \n\n',['1','2'])
             if question == '1':
                 idk()
             elif question == '2':
-                serious_level = int(input('\nHow serious was it on a scale of 1-5? (Put 0 to see what the number on the scale means) \n\n'))
-                if serious_level in range(1, 3):
-                    idk()
-                elif serious_level in range(3, 6):
-                    calculate_cady_law()
-                elif serious_level == 0:
-                    print('\n1-2: They went on one bad date and are not attracted to each other at all. \n')
-                    print('\n3: They dated for a couple weeks and it didn\'t get serious. \n')
-                    print('\n4-5: They dated very seriously; they met each others parents; their relationship lasted over three months; they maybe have a kid and/or pet together; they talked about their future plans together. \n')
-                else:
-                    print('\nPlease put a number between 1-5. \n')
+                while True:
+                    serious_level = int(input('\nHow serious was it on a scale of 1-5? (Put 0 to see what the number on the scale means) \n\n'))
+                    match serious_level:
+                        case n if n in range(1, 3):
+                            idk()
+                            break
+                        case n if n in range(3, 6):
+                            calculate_cady_law()
+                            break
+                        case 0:
+                            print('\n1-2: They went on one bad date and are not attracted to each other at all. \n')
+                            print('\n3: They dated for a couple weeks and it didn\'t get serious. \n')
+                            print('\n4-5: They dated very seriously; they met each others parents; their relationship lasted over three months; they maybe have a kid and/or pet together; they talked about their future plans together. \n')
+                            continue
+                        case _:
+                            print('\nPlease put a number between 1-5. \n')
+                            continue
         elif dated_in_group == 'no':
             friend_likes_your_crush = ask_question('\nDoes anyone else in your friend group like your crush? \n\n',['yes','no'])
             if friend_likes_your_crush == 'yes':
@@ -138,8 +141,22 @@ def idrk():
                     idek()
     elif in_group == 'no':
         print('\nTell them, but be mentally prepared. \n')
-        time.sleep(3)
-        exit()
+        end()
+
+def i_ran_out_of_name_ideas_lol():
+    mutiple = ask_question('\nSame person (1) or multiple people? (2) \n\n',['1','2'])
+    if mutiple == '2':
+        print('\nTell them, but it depends what you want out of this. \n')
+        end()
+    if mutiple == '1':
+        print('\nOk they\'re in a situationship')
+        role = ask_question('\nWhat\'s their role in the situationship? (They\'re in a situationship and they\'re being dragged along by the person who won\'t commit (1) or they\'re in a situationship and they\'re the one who won\'t commit and is dragging the other person along (2).) \n\n',['1','2'])
+        if role == '2':
+            print('\nYou should tell your crush you like them, but it\'s likely they\'ll be too blinded by that other person to reciprocate your feelings. \n')
+            end()
+        if role == '1':
+            print('\nDon\'t tell them unless you also want to be in a situationship. \n')
+            end()
 
 def irdk():
     do_you_like_them = ask_question('\nDo you actually like them (1) or do you only have a crush on them because you\'re in a forced proximity with them? (2) \n\n',['1','2'])
@@ -151,6 +168,7 @@ def irdk():
             no2()
         elif you_sure == 'yes':
             print('\nTell them (either the best relationship of yoru life and you\'ll be married or make things incredibly awkward and uncomfortable for yourself everyday when you\'re forced to see them. \n)')
+            end()
 
 def main():
     while True:
@@ -158,17 +176,42 @@ def main():
         match question:
             case 'idk':
                 print('\nFind out. \n')
-                time.sleep(3)
-                exit()
+                end()
+            case 'yes':
+                define_seeing = ask_question('Are they casually dating someone (1) or are they in a relationship together (2) or are they hooking up with someone? (3) \n\n',['1','2','3'])
+                if define_seeing == '3':
+                    i_ran_out_of_name_ideas_lol()
+                elif define_seeing == '2':
+                    no()
+                elif define_seeing == '1':
+                    casual = ask_question('\nIs it actually casual? \n\n',['yes','no'])
+                    if casual == 'no':
+                        no()
+                    elif casual == 'yes':
+                        i_ran_out_of_name_ideas_lol()
             case 'no':
                 question = ask_question('\nHow do you know your crush? (Online or in person?) \n\n', ['online', 'in person'])
                 if question == 'online':
-                    print('\nPlaceholder for online scenario. \n')
+                    celebrity = ask_question('\nAre they a celebrity? \n\n',['yes','no'])
+                    if celebrity == 'yes':
+                        print('\nYou can tell them, but they\'re never gonna know about it. \n')
+                        end()
+                    elif celebrity == 'no':
+                        dating_app = ask_question('\nDid you meet this person on a dating app? \n\n',['yes','no'])
+                        if dating_app == 'yes':
+                            print('\nYea, you should tell them, but I already assume they already know you like them (assuming you matched). \n')
+                        elif dating_app == 'no':
+                            rl = ask_question('\nHave you ever met in real life? \n\n',['yes','no'])
+                            if rl == 'yes':
+                                yes()
+                            elif rl == 'no':
+                                print('\nYou can tell them, but there\'s a chance you won\'t be attracted to them irl. \n')
+                                end()
                 elif question == 'in person':
                     question = ask_question('\nHow did you meet your crush? (Friends, school, work, other) \n\n', ['friends', 'school', 'work', 'other'])
                     match question:
                         case 'friends':
-                            response = ask_question('\nAre you friends with your crush (1) or did you meet via friends (2)? \n\n', ['1', '2'])
+                            response = ask_question('\nAre you friends with your crush (1) or did you meet via friends? (2) \n\n', ['1', '2'])
                             if response == '1':
                                 idrk()
                             elif response == '2':
@@ -182,8 +225,7 @@ def main():
                                         are_you_sure = ask_question('Do you think you like them just because they\'re your friend\'s sibling? \n\n')
                                         if are_you_sure == 'yes':
                                             print('\nFor the love of god, don\'t tell them, like that\'s actually wildddddddddddd. \n')
-                                            time.sleep(3)
-                                            exit()
+                                            end()
                                         elif are_you_sure == 'no':
                                             why_are_you_going_down_this_path_anyway = ask_question('\nDoes the prospect of being with your crush outweight the fact that you may ruin/make awkward (at least for a time) your relationship with your friend? \n\n',['yes','no'])
                                             if why_are_you_going_down_this_path_anyway == 'no':
@@ -225,14 +267,15 @@ def main():
                                                     break
                                             print('\nOfc you have a werid thing for authority figures which is why your here in the first place, but that\'s not the point. \n')
                                             print('\nIf you do it, there is a one percent chance you\'ll get a promotion and a ninety-nine percent chance you\'ll be fired immediately. \n')
+                                            end()
                                         elif surperior == 'no':
-                                            cooking = ask_question('\n')
+                                            cooking = ask_question('\nIs there any chance at all your crush actually likes you (1) or is this some sick power play that will make your crush incredibly uncomfortable for the rest of their limited time working there before they inevitably resign? (2) \n\n',['1','2'])
+                                            if cooking == '2':
+                                                no()
+                                            elif cooking == '1':
+                                                print('\nHR will hear about this. \n')
+                                                end()
                         case 'other':
-                            print('\nPlaceholder for other scenario. \n')
-            case 'yes':
-                print('\nCooked, move on. Why were you liking them in the first place? \n')
-                time.sleep(3)
-                exit()
-
+                            yes()
 if __name__ == "__main__":
     main()
